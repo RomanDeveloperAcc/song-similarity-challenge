9fd9df9191fa3d3d6b25f44c4469bea93f774517
RomanDeveloperAcc <roma04dan@gmail.com>
2020-08-31 20:19:06 +0300

    added router transitions

--- my-app/src/app/components/songs/songs.component.ts
+++ my-app/src/app/components/songs/songs.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { fader } from './songs-routing.animations';
 
 @Component({
   selector: 'app-songs',
   templateUrl: './songs.component.html',
-  styleUrls: ['./songs.component.scss']
+  styleUrls: ['./songs.component.scss'],
+  animations: [fader]
 })
 export class SongsComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public prepareRoute(outlet: RouterOutlet): any {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation;
   }
 
 }
