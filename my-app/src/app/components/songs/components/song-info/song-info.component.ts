9ea90a6bdf0e9243cd8245876d1437e86810c3e3
RomanDeveloperAcc <roma04dan@gmail.com>
2020-08-31 13:18:00 +0300

    added a possibility to close popup from itself

--- my-app/src/app/components/songs/components/song-info/song-info.component.ts
+++ my-app/src/app/components/songs/components/song-info/song-info.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 import { SimilarSongModel } from '../../models/similar-song.model';
 import { SongsService } from '../../services/songs.service';
 
@@ -9,11 +10,17 @@ import { SongsService } from '../../services/songs.service';
 })
 export class SongInfoComponent implements OnInit {
   public songInfo: SimilarSongModel;
+  public crossImage = '/assets/img/songs/cross.png';
 
-  constructor(private songsService: SongsService) { }
+  constructor(private songsService: SongsService,
+              private matDialogRef: MatDialogRef<SongInfoComponent>) { }
 
   ngOnInit(): void {
     this.setSongInfo();
+  }
+
+  public closeDialogWindow(): void {
+    this.matDialogRef.close();
   }
 
   private setSongInfo(): void {
